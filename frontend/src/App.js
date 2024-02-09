83de167cbf0bc7616f6dce546fd4ae40a357030d
Mark Zhdan <markzhdan@gmail.com>
2024-02-09 17:58:03 -0600

    Added Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Frontpage from "./pages/Frontpage/Frontpage";
+import Navbar from "./components/navbar/Navbar";
+
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Journal from "./pages/Journal/Journal";
+import Therapist from "./pages/Therapist/Therapist";
+
+import Missing from "./pages/Missing/Missing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Frontpage />} />
+
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/journal" element={<Journal />} />
+          <Route path="/therapist" element={<Therapist />} />
+
+          <Route path="*" element={<Missing />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Therapist/Therapist.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Therapist = () => {
+  return <div>Therapist</div>;
+};
+
+export default Therapist;
