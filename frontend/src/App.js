c1dc85289f12d70097f62791fd62bbc854a8f143
Mark Zhdan <markzhdan@gmail.com>
2024-02-09 17:14:03 -0600

    Initial Commmit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
