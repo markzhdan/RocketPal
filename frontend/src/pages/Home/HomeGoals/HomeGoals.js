b51785d8eba3c9eb578c573629b2adafec6ae490
Shrut <shrutpatel20@gmail.com>
2024-02-10 01:10:56 -0600

    Add goal functions

--- frontend/src/pages/Home/HomeGoals/HomeGoals.js
+++ frontend/src/pages/Home/HomeGoals/HomeGoals.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./HomeGoals.css";
 import { Checkbox } from "@nextui-org/react";
+import { FaBlackTie, FaTrashCan } from "react-icons/fa6";
+import { FaTrash } from "react-icons/fa";
+import { Input } from "@nextui-org/react";
+import { IoIosAddCircle } from "react-icons/io";
 
 const HomeGoals = () => {
   const [goals, setGoals] = useState([
@@ -75,6 +79,11 @@ const HomeGoals = () => {
     },
   ]);
 
+  const handleDelete = (goalName) => {
+    const updatedGoals = goals.filter((goal) => goal.name !== goalName);
+    setGoals(updatedGoals);
+  };
+
   const handleTaskClick = (goalName, taskName) => {
     const updatedGoals = goals.map((goal) => {
       if (goal.name === goalName) {
@@ -98,41 +107,57 @@ const HomeGoals = () => {
   console.log(goals);
 
   return (
-    <>
-      {goals.length == 0 ? (
-        <main className="HomeGoals">
-          <h1>You completed all your goals, add button</h1>
-        </main>
-      ) : (
-        <main className="HomeGoals">
-          <h1 className="Head">Goals</h1>
-          <section className="Goals">
-            <div className="goal-container">
-              {goals.map((goal) => (
-                <div className="Goal" key={goal.name}>
-                  <h2>{goal.name}</h2>
-
-                  <div className="Tasks">
-                    {goal.tasks.map((task) => (
-                      <div className="Task" key={task.name}>
-                        <Checkbox
-                          color="success"
-                          isSelected={task.completed}
-                          onChange={() => handleTaskClick(goal.name, task.name)}
-                          lineThrough={task.completed}
-                        >
-                          {task.name}
-                        </Checkbox>
-                      </div>
-                    ))}
+    <main className="HomeGoals">
+      <h1 className="Head">Goals</h1>
+      <section className="Goals">
+        <div className="goal-container">
+          {goals.map((goal) => (
+            <div className="Goal" key={goal.name}>
+              <div className="Goal-Heading">
+                <h2>{goal.name}</h2>
+                <FaTrash
+                  onClick={() => handleDelete(goal.name)}
+                  role="button"
+                  tabIndex="0"
+                />
+              </div>
+              <div className="Tasks">
+                {goal.tasks.map((task) => (
+                  <div className="Task" key={task.name}>
+                    <Checkbox
+                      color="success"
+                      isSelected={task.completed}
+                      onChange={() => handleTaskClick(goal.name, task.name)}
+                      lineThrough={task.completed}
+                    >
+                      {task.name}
+                    </Checkbox>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </section>
-        </main>
-      )}
-    </>
+          ))}
+
+          <div
+            className="Add-Container"
+            style={{ visibility: goals.length >= 3 ? "hidden" : "visible" }}
+          >
+            <Input
+              type="Goals"
+              label="Add Goal"
+              placeholder="Enter a Goal"
+              classNames={{
+                input: ["placeholder:text-black"],
+              }}
+            />
+            <IoIosAddCircle
+              style={{ fontSize: "40px" }}
+              onClick={() => handGoalClick(goalName)}
+            />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
