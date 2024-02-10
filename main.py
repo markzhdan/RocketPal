b94d9f7edd576464b466db24d4dc4988fe9db964
Jae Park <parkjae433@gmail.com>
2024-02-09 18:04:27 -0600

    file structure

---
+++ main.py
@@ -0,0 +1,22 @@
+rocket = "rocket"
+
+rocket = 1.234
+
+rocket = True
+
+def rocket(rocket1, rocket2):
+    if rocket1 == "marcus":
+        print('marcus')
+    elif rocket == "vince":
+        print('vince')
+    else:
+        print(rocket2)
+
+rocket("marcus", "hello")
+
+lst = [1, 2, 3, 4, 5, 6, 7]
+
+dick = {"hello" : "OI OI", "luffy" : 123}
+
+
+print(dick['hello'])
