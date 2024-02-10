a70372943c022234efc156846602e4ee192757b7
Shrut <shrutpatel20@gmail.com>
2024-02-10 14:16:17 -0600

    Added frontpage

--- frontend/src/pages/Frontpage/Frontpage.js
+++ frontend/src/pages/Frontpage/Frontpage.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Frontpage.css";
-
+import { Button, ButtonGroup } from "@nextui-org/react";
 import rocket from "../../assets/rocket4.gif";
+import { Link } from "react-router-dom";
 
 const Frontpage = () => {
   return (
     <main className="Frontpage">
       <img src={rocket} alt="Rocketship Animation" width={800} />
+      <div className="container">
+        <p className="typed">Where Your Goals Will Reach the Stars...</p>
+      </div>
+      <div className="button-container">
+        <Link to={`/Login`} className="Sign-Up">
+          <Button color="primary" size="lg">
+            Get Started!
+          </Button>
+        </Link>
+      </div>
     </main>
   );
 };
--- frontend/src/pages/Missing/Missing.js
+++ frontend/src/pages/Missing/Missing.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Missing.css"; // Import CSS file for styling
 
 const Missing = () => {
-  return <main className="Missing">Missing</main>;
+  return (
+    <main className="Missing">
+      <div className="container">
+        <h1>404 - Page Not Found</h1>
+        <div className="container">
+          <p className="typed">We're sorry, the page you are looking for might have been removed, had
+          its name changed, or is temporarily unavailable.</p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Missing;
